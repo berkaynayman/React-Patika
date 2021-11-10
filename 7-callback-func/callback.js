89dc49d39a672c1d22e47545410efb26aa7f5738
berkaynayman <berkaynayman4@gmail.com>
2021-11-10 14:15:00 +0300

    callback func - async,await

---
+++ 7-callback-func/callback.js
@@ -0,0 +1,93 @@
+/*
+setTimeout(() => {
+    console.log("merhaba")
+}, 4000)
+
+setInterval(() => {
+    console.log("merhaba ben her saniye çalışacağım")
+}, 1000)
+*/
+
+/*
+const sayHi = (cb) => {
+    cb();
+}
+sayHi(() => {
+    console.log("hello");
+})
+*/
+
+import fetch from "node-fetch";
+
+/*
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then((data) => data.json())
+    .then((users) => console.log(users))
+*/
+
+/*
+fetch("https://jsonplaceholder.typicode.com/users/1")
+    .then((data) => data.json())
+    .then((user) => {
+        console.log(`USER YÜKLENDİ ${user}`)
+
+        fetch("https://jsonplaceholder.typicode.com/posts/1")
+            .then((data) => data.json())
+            .then((post) => {
+                console.log(`POST YÜKLENDİ ${post}`)
+
+                fetch("https://jsonplaceholder.typicode.com/posts/2")
+                    .then((data) => data.json())
+                    .then((post2) => console.log(`POST 2 YÜKLENDİ ${post2}`))
+            });
+    });
+*/
+
+/*
+async function getData(){
+    const user = await(
+        await fetch("https://jsonplaceholder.typicode.com/users/1")
+    ).json();
+
+    const post1 = await(
+        await fetch("https://jsonplaceholder.typicode.com/posts/1")
+    ).json();
+
+    const post2 = await(
+        await fetch("https://jsonplaceholder.typicode.com/posts/2")
+    ).json();
+
+    console.log(`USER YUKLENDİ ${user}`);
+    console.log(user);
+
+    console.log(`POST1 YUKLENDİ ${post1}`);
+    console.log(post1);
+
+    console.log(`POST2 YUKLENDİ ${post2}`);
+    console.log(post2);
+}
+getData()
+*/
+
+(async() => {
+    const user = await(
+        await fetch("https://jsonplaceholder.typicode.com/users/1")
+    ).json();
+
+    const post1 = await(
+        await fetch("https://jsonplaceholder.typicode.com/posts/1")
+    ).json();
+
+    const post2 = await(
+        await fetch("https://jsonplaceholder.typicode.com/posts/2")
+    ).json();
+
+    console.log(`USER YUKLENDİ ${user}`);
+    console.log(user);
+
+    console.log(`POST1 YUKLENDİ ${post1}`);
+    console.log(post1);
+
+    console.log(`POST2 YUKLENDİ ${post2}`);
+    console.log(post2);
+})()
